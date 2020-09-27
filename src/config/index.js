51585f9e935d1b94b1c765a1715f5c7d3db7094b
myrowtj0827 <mnblkjpoi0324@hotmail.com>
2020-09-27 19:51:29 +0200

    path fixing

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export default {
-    SIM_API_URL: "http://127.0.0.1:8050/",
-    ///SIM_API_URL: "https://scrapingapi.herokuapp.com/",
+    //SIM_API_URL: "http://127.0.0.1:5000/",
+    SIM_API_URL: "https://shoping-ecommerce.herokuapp.com/",
 };
